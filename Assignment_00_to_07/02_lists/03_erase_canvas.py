d029930eee7f6cacb8d1eaac0877bcb480df42b3
laibakhab <asifalikhan4485@gmail.com>
2025-05-07 00:04:39 +0500

    03

--- Assignment_00_to_07/02_lists/03_erase_canvas.py
+++ Assignment_00_to_07/02_lists/03_erase_canvas.py
@@ -2,3 +2,19 @@
 # Implement an 'eraser' on a canvas.
 
 # The canvas consists of a grid of blue 'cells' which are drawn as rectangles on the screen. We then create an eraser rectangle which, when dragged around the canvas, sets all of the rectangles it is in contact with to white.
+
+import pygame
+import time
+
+pygame.init()
+
+CANVA_WIDTH = 400
+CANVA_HEIGHT = 400
+CELL_SIZE = 40
+ERASER_SIZE = 20
+
+BLUE = (0,0,225)
+WHITE = (255,255,255)
+PINK = (225,182,193)
+
+screen = pygame.display.set_mode(CANVA_WIDTH , CANVA_HEIGHT)
--- Assignment_00_to_07/02_lists/08_shorten.py
+++ Assignment_00_to_07/02_lists/08_shorten.py
@@ -1,4 +1,23 @@
 # Problem Statement
 # Fill out the function shorten(lst) which removes elements from the end of lst, which is a list, and prints each item it removes until lst is MAX_LENGTH items long. If lst is already shorter than MAX_LENGTH you should leave it unchanged. We've written a main() function for you which gets a list and passes it into your function once you run the program. For the autograder to pass you will need MAX_LENGTH to be 3, but feel free to change it around to test your program.
 
+print("shorten")
 
+MAX_LENGH: int = 3
+def shorten(lst):
+    while len(lst) >= MAX_LENGH:
+        last_element = lst.pop()
+        print(last_element)
+def get_lst():
+    lst = []
+    element = input('Enter an element to add to the list : ')
+    while element != '':
+        lst.append(element)
+        element = input('Enter an element to add to the list : ')
+    return lst
+def main():
+    lst = get_lst()
+    shorten(lst)
+
+if __name__ == '__main__':
+    main()        
